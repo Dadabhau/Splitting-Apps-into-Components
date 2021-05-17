8e5b6c41d6b718819df5a37a5a104f008cf6d04f
dadabhau <dadabhaut@ithena.ai>
2021-05-17 16:18:03 +0530

    encapsulation added for css

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit,ViewEncapsulation } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
-  styleUrls: ['./server-element.component.css']
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.Emulated //None, Native
 })
 export class ServerElementComponent implements OnInit {
   // Data received in this format into app (Parent Component)
